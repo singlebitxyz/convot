05f53e1f46b4d9285bc33fe8f9f547136b24be9c
Naman <naman.barkiya02@gmail.com>
2025-11-23 20:28:07 +0530

    cookie options vuln fixes

--- frontend/lib/supabase/client.ts
+++ frontend/lib/supabase/client.ts
@@ -3,16 +3,6 @@ import { createBrowserClient } from "@supabase/ssr";
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookieOptions: {
-        name: "sb-auth-token",
-        maxAge: 60 * 60 * 24 * 7,
-        domain: "",
-        path: "/",
-        sameSite: "none",
-        secure: true,
-      },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
